11c3619e3b177be38bc8e406df2f1653f8874b3f
CompetitiveBit8 <babatundeksamuel@gmail.com>
2026-02-22 19:55:53 +0100

    Update

--- app/auth_database.py
+++ app/auth_database.py
@@ -7,7 +7,6 @@ SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///app.db"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-
 class Base(DeclarativeBase):
     pass
 
